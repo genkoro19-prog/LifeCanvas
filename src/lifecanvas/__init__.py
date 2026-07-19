bb58b41293e0a0816853820af475a67c15807931
genkoro19-prog <genkoro19@gmail.com>
2026-07-19 19:03:25 +0900

    Expose LifeCanvas public API

--- src/lifecanvas/__init__.py
+++ src/lifecanvas/__init__.py
@@ -2,6 +2,6 @@
 
 from .engine import SimulationEngine
 from .models import ProjectPlan, YearResult
-from .sample import build_genki_family_plan
+from .sample import build_genki_family_plan, create_sample_plan
 
-__all__ = ["SimulationEngine", "ProjectPlan", "YearResult", "build_genki_family_plan"]
+__all__ = ["SimulationEngine", "ProjectPlan", "YearResult", "build_genki_family_plan", "create_sample_plan"]
