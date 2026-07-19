9059455b89b4ae12a92c8ec4b1f869a9b208eef0
genkoro19-prog <genkoro19@gmail.com>
2026-07-19 19:50:53 +0900

    Use enhanced direct-input UI

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 
 sys.path.insert(0, str(Path(__file__).resolve().parent / "src"))
 
-from lifecanvas.ui import run_app
+from lifecanvas.enhanced_ui import run_app
 
 
 if __name__ == "__main__":
